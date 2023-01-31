07830093f95aeed3ee84c83282659e076de39122
cervantes21 <admon.owlsoul@gmail.com>
2023-01-31 01:41:59 -0600

    Update hands_detector

--- hands_detector.py
+++ hands_detector.py
@@ -1,27 +1,50 @@
 import cv2
 import mediapipe as mp
+# Importamos las Librerías a usar.
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils # Dibuja los resultados de las detecciones.
+mp_hands = mp.solutions.hands # Es un modelo para la detección de las manos.
 
-cap = cv2.VideoCapture(0) # Para hacer Streaming
+# Llamamos a los metódos de mediapipe. 
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # Para hacer uso de la webcam
 
 with mp_hands.Hands(
+    # Entramos a la configuración de mediapipe-hands.
         static_image_mode=False,
+        # Puede tener valores de True, o False. False para Streaming y True para imagénes estáticas, o cambiantes.
         max_num_hands=2,
+        # El número máximo de manos a detectar.
         min_detection_confidence=0.5) as hands:
-
+        # Valor minimo de confianza. Nos ayuda para saber si la detección es exitosa.
         while True:
+            # Comenzamos con el ciclo para leer los fotogramas.
             ret, frame = cap.read()
             if ret == False:
                 break
 
             height, width, _ = frame.shape
+            # Pedimos el alto y ancho del fotograma
             frame = cv2.flip(frame,1)
-
+            # Lo volteamos para efecto tipo espejo.
+            
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # Transformamos los fotogramas a RGB
+            
+            results = hands.process(frame_rgb)
+            
+            if results.multi_hand_landmarks is not None:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        frame,
+                        hand_landmarks,
+                        mp_hands.HAND_CONNECTIONS
+                    )
+                                            
             cv2.imshow("Frame", frame)
+            # Mostramos el vídeo.
             q = cv2.waitKey(30)
             if q == 27: # 27 es el ascii para ESC
-                break
+                break # Función para Salir de la aplicación con Esc.
 cap.release()
-
+cv2.destroyAllWindows()
